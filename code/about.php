e9f725f2738ca2b0d1255890c44bc63ab04424ca
sarahlars <56215770+sarahlars@users.noreply.github.com>
2020-10-14 18:43:43 -0600

    converted files to php

--- code/about.php
+++ code/about.php
@@ -32,26 +32,7 @@
 <body>
 
     <header>
-        <!--Navigation
-        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top">
-            <a href="beehivestorage-utah.com/test" class="navbar-brand mr-auto pl-1">Beehive Storage</a>
-            <button class="navbar-toggler rounded" data-toggle="collapse" data-target="#navContent"
-                aria-controls="navBarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
-                    class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="index.php" title="ABOUT"> Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="locationsMain.php" title="Beehive Storage Locations">
-                            Locations</a></li>
-                    <li class="nav-item"><a class="nav-link" href="about.php" title="About Beehive Storage"> About</a>
-                    </li>
-                    <li class="nav-item"><a class="nav-link" href="coupons.php" title="Coupons"> Coupons</a></li>
-                    <li class="nav-item"><a class="nav-link" href="photos.php" title="Photo Gallery"> Photo Gallery</a>
-                    </li>
-
-                </ul>
-            </div>
-        </nav> -->
+        
         <div class="container-fluid page-header row col-8 fixed-top" style="padding-bottom: 10%;">
 
             <!-- <div class="container-fluid  col-lg" id="motto">
--- code/navbar.php
+++ code/navbar.php
@@ -1,18 +1,35 @@
-<header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top">
-            <a href="#" class="navbar-brand mr-auto pl-1">Beehive Storage</a>
-            <button class="navbar-toggler rounded" data-toggle="collapse" data-target="#navContent"
-                aria-controls="navBarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
-                    class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="index.php" title="ABOUT"> Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="locationsMain.php" title="Beehive Storage Locations"> Locations</a></li>
-                    <li class="nav-item"><a class="nav-link" href="about.php" title="About Beehive Storage"> About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="coupons.php" title="Coupons"> Coupons</a></li>
-                    <li class="nav-item"><a class="nav-link" href="photos.php" title="Photo Gallery"> Photo Gallery</a></li>
+<nav class="navbar navbar-expand-lg navbar-light  " role="navigation">
+            <a href="beehivestorage-utah.com/test" class="navbar-brand mr-auto pl-1 position-fixed"><img
+                    src="../logo.png">
+            </a>
 
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="navbar-nav mr-auto flex-column vertical-nav">
+                    <li class="nav-item ">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="locationsMain.php">Locations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="coupons.php">Coupons</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="photos.php">Photo Gallery</a>
+                    </li>
                 </ul>
+
             </div>
+
         </nav>
-    </header>
