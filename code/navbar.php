681207aa5d2fa62e724fcb01d4fa1d7548ae3afd
sarahlars <56215770+sarahlars@users.noreply.github.com>
2020-10-21 20:00:12 -0600

    nixed the about page

--- code/navbar.php
+++ code/navbar.php
@@ -1,30 +1,47 @@
  <!-- New Navbar Code  -->
 <header>
+
         <div class="container-fluid container p-3 bg-warning text-white text-center col-12">
             <span>
                 <h1>Beehive Storage Gallery</h1>
-                <p>Low prices and Competitive Pricing</p>
+                <p>Low prices and Competitve Pricing</p>
             </span>
         </div>
 
-         <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top ">
-             
-            <a href="beehivestorage-utah.com/test" class="navbar-brand mr-auto pl-1 " id="logoHeader"><img src="logo.png">
+
+
+    
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top ">
+            <a href="beehivestorage-utah.com/test" class="navbar-brand mr-auto pl-1" id="logoHeader"><img
+                    src="logo.png">
             </a>
+
             <button class="navbar-toggler rounded" data-toggle="collapse" data-target="#navContent"
                 aria-controls="navBarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
                     class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navContent">
+            <div class="collapse navbar-collapse " id="navContent">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link" href="beehivestorage-utah.com" title="Home"> Home</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="#" title="About"> About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" title="Locations">Locations</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" title="Photo Gallery">Photo Gallery</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" title="Coupons">Coupons</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="#" title="About"> About</a></li> -->
+                    <li class="nav-item"><a class="nav-link" href="#" title="Locations"> Locations</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" title="Photo Gallery"> Photo Gallery</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" title="Coupons"> Coupons</a></li>
                 </ul>
             </div>
 
         </nav>
+       
 
-</header>
+        <!--End navigation-->
+        <br />
+
+
+
+
+
+
+
+
+
+    </header>
