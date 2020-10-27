66713db801218b4e7525eb6f5a6d18c9ee678bf1
sarahlars <56215770+sarahlars@users.noreply.github.com>
2020-10-26 18:05:23 -0600

    new photo folder

--- code/navbar.php
+++ code/navbar.php
@@ -13,20 +13,18 @@
     
         <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top ">
             <a href="beehivestorage-utah.com/test" class="navbar-brand mr-auto pl-1" id="logoHeader"><img
-                    src="logo.png">
+                    src="../logo.png">
             </a>
 
             <button class="navbar-toggler rounded" data-toggle="collapse" data-target="#navContent"
                 aria-controls="navBarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
                     class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse " id="navContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="beehivestorage-utah.com" title="Home"> Home</a>
-                    </li>
-                    <!-- <li class="nav-item"><a class="nav-link" href="#" title="About"> About</a></li> -->
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a class="nav-link" href="beehivestorage-utah.com" title="Home"> Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="#" title="Locations"> Locations</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" title="Photo Gallery"> Photo Gallery</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#" title="Coupons"> Coupons</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" title="Photo Gallery">Photo Gallery</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" title="Coupons">Coupons</a></li>
                 </ul>
             </div>
 
